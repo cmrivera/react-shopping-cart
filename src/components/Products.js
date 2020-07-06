9a92f7666fd1567b3bdf657272b7caf61feb1e1f
cmrivera <cmrivera0004@student.stcc.edu>
2020-07-06 12:27:10 -0400

    Step 4 passing data with ProductsContext

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, { useContext } from "react";
+import ProductContext from "../contexts/ProductContext";
 
 // Components
-import Product from './Product';
+import Product from "./Product";
 
-const Products = props => {
-	return (
-		<div className="products-container">
-			{props.products.map(product => (
-				<Product
-					key={product.id}
-					product={product}
-					addItem={props.addItem}
-				/>
-			))}
-		</div>
-	);
+const Products = () => {
+  /*When we do this, `useContext` is going to return value passed by our `ProductContext` Provider `value` prop. In our case we're getting back an object with two properties. A `products` property and a `addItem` property. We can go ahead and destructure those. */
+
+  const { products, addItem } = useContext(ProductContext);
+
+  /*Now that we have all of the data we need we can refactor our `Products` component from using props.
+
+- To do so we just need to remove every instance of `props`.
+
+  - Remove it from the function parameters
+  - Remove it from the products map
+	- Remove it from addItem prop */
+
+  return (
+    <div className="products-container">
+      {products.map((product) => (
+        <Product key={product.id} product={product} addItem={addItem} />
+      ))}
+    </div>
+  );
 };
-
+//Now our `Products` component is getting it's data solely from `Context API`
 export default Products;
