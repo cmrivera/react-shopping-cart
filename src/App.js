0e5a3835637ad8d174e8ba5dfd7335f09eaf8f09
cmrivera <cmrivera0004@student.stcc.edu>
2020-07-06 12:47:41 -0400

    Step 6 & 7 passing data (SC, Nav) use CartContext.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Route } from "react-router-dom";
 import data from "./data";
 import ProductContext from "./contexts/ProductContext";
+import CartContext from "./contexts/CartContext";
 
 // Components
 import Navigation from "./components/Navigation";
@@ -16,20 +17,24 @@ function App() {
     // add the given item to the cart
     setCart([...cart, item]);
   };
+  //wrap the following in ProductContext.Provider for root provider
+  //then wrap components in CartContext.Provider
 
   return (
     <div className="App">
       <ProductContext.Provider value={{ products, addItem }}>
-        <Navigation cart={cart} />
+        <CartContext.Provider value={{ cart }}>
+          <Navigation cart={cart} />
 
-        {/* Routes */}
-        <Route exact path="/">
-          <Products />
-        </Route>
+          {/* Routes */}
+          <Route exact path="/">
+            <Products />
+          </Route>
 
-        <Route path="/cart">
-          <ShoppingCart cart={cart} />
-        </Route>
+          <Route path="/cart">
+            <ShoppingCart cart={cart} />
+          </Route>
+        </CartContext.Provider>
       </ProductContext.Provider>
     </div>
   );
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useContext } from "react";
+import { NavLink } from "react-router-dom";
+import CartContext from "../contexts/CartContext";
 
-const Navigation = props => {
-	return (
-		<div className="navigation">
-			<NavLink to="/">Products</NavLink>
-			<NavLink to="/cart">
-				Cart <span>{props.cart.length}</span>
-			</NavLink>
-		</div>
-	);
+/* Time to do the same thing for our `Navigation` component.
+  - First import the `useContext` hook and our `CartContext`
+  - Next, pass our `CartContext` to the `useContext` hook and assign it to a variable named cart.
+  - Lastly we need to remove all instances of `props`
+    - Remove `props` from our parameters
+    - Remove `props` from our cart length
+     */
+
+const Navigation = () => {
+  const { cart } = useContext(CartContext);
+
+  return (
+    <div className="navigation">
+      <NavLink to="/">Products</NavLink>
+      <NavLink to="/cart">
+        Cart <span>{cart.length}</span>
+      </NavLink>
+    </div>
+  );
 };
 
 export default Navigation;
